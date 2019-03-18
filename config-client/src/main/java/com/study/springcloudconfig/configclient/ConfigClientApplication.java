7a80e2064cb194053bf96df0e20c44dfa7e99b1e
zhaopinchao <17688786060@sina.cn>
2019-03-18 21:39:50 +0800

    Initial commit

---
+++ config-client/src/main/java/com/study/springcloudconfig/configclient/ConfigClientApplication.java
@@ -0,0 +1,23 @@
+package com.study.springcloudconfig.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer c = new PropertySourcesPlaceholderConfigurer();
+        c.setIgnoreUnresolvablePlaceholders(true);
+        return c;
+    }
+}
